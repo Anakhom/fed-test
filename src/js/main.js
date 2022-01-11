3e658d0488d27959fa34e2dc3efd6480170c76bb
Anastasia K <anastasia.khomchenko@gmail.com>
2022-01-11 13:45:39 -0500

    assessment done

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,18 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
+                        '<div class="reports_div">' +
                             '<h3 class="reports_title">' +
                                 '<a href="{{url}}" target="_blank">{{title}}</a>' +
                             '</h3>' +
+                            '<h2 class="reports_size_type">' +
+                                '({{file_size}} {{file_type}})' +
+                            '</h2>' +
+                        '</div>'+
                         '{{/documents}}' +
                     '</footer>' +
                 '</article>' +
